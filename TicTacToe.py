b764c89d9c97fa05de53dce9b3552815c3b68164
Piankov <piankovpe@gmail.com>
2022-11-28 19:26:00 +0600

    add history and history clearing

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,3 +1,4 @@
+import shutil
 import tkinter
 from tkinter import *
 import random
@@ -6,7 +7,7 @@ import os
 from tkinter import ttk
 
 global PLAYER, PLAYER_IMAGE, AI_IMAGE  # PLAYER = 4: X-win, PLAYER = 5: O-win.
-global GAME_NUM, TURN_NUM
+global GAME_NUM, TURN_NUM, HISTORY_TURN, HISTORY_INDEX
 history_list = []
 
 
@@ -220,63 +221,78 @@ def hide_tiles():
 
 
 def menu():
+    main_w.config(padx=145,
+                  pady=45, )
+    for widget in main_w.winfo_children():
+        widget.grid_remove()
     for widget in main_w.winfo_children()[10:14]:
         widget.grid()
+    history_list.clear()
+    history_choose_game.configure(values=[])
+    # deleting all games from history which was created from screenshot #
+    # btw it's not deleting shots which was made and program closed #
+    [os.listdir(f"history/{element}") if len(os.listdir(f"history/{element}")) > 4
+     else shutil.rmtree(f"history/{element}") for element in os.listdir("history")]
+    # changed protocol line: 331#
 
 
 def history():
-    main_w.config(padx=0)
-    for widget in main_w.winfo_children()[10:14]:
-        widget.grid_remove()
-    played = [int(element.strip("Game")) for element in os.listdir('history')]  # all games
-    played.sort()
-    game_index = played[-1]
-    all_turns = os.listdir(f"history/Game{game_index}")
-    for index in all_turns:
-        path = f"history/Game{played[0]}/{index}"
-        history_list.append(tkinter.PhotoImage(file=path))
-    print(history_list)
-    history_label = Canvas(main_w,
-                           width=525,
-                           height=520)
+    global HISTORY_TURN, HISTORY_INDEX
+    # change all widgets to list maybe #
+    # if I don't want to make other window to show it, I need to create all widget's out of the function
+    # otherwise it will change winfo_children() order!
+    try:
+        for widget in main_w.winfo_children():
+            widget.grid_remove()
+        main_w.config(padx=75)
+        history_go_back_button.grid()
+        history_label.grid()
+        history_choose_game.grid()
+        history_prev_turn_button.grid()
+        history_next_turn_button.grid()
+        played = [int(element.strip("Game")) for element in os.listdir('history')]  # all games
+        played.sort()
+        HISTORY_INDEX = played[-1]  # last game played
+        all_turns = os.listdir(f"history/Game{HISTORY_INDEX}")
+        HISTORY_TURN = 0
+        # trash collector deletes created photos if they're not stored out of function
+        for index in all_turns:
+            path = f"history/Game{HISTORY_INDEX}/{index}"
+            history_list.append(tkinter.PhotoImage(file=path))
+        history_label.create_image(0, 0, image=history_list[HISTORY_TURN], anchor=NW)
+        history_choose_game.configure(values=[str(f"Game: {value}") for value in played])
+        history_choose_game.set(f"Game: {played[-1]}")
+    except FileNotFoundError:
+        menu()
 
-    history_label.grid(row=0,
-                       column=1)
-    go_back_history = Button(main_w,
-                             image=go_back_icon,
-                             highlightthickness=0,
-                             border=0,
-                             relief=tkinter.RIDGE,
-                             )
-    go_back_history.grid(row=0,
-                         column=2,
-                         sticky="se")
-    history_label.create_image(0, 0, image=history_list[0], anchor=NW)
-    go_back_history.lower()
-    style = ttk.Style()
-    style.configure("TCombobox", fieldbackground="white", foreground="black",
-                    background="white", highlightthickness=0, border=0, highlightcolor="white")
-    choose_game = ttk.Combobox(main_w,
-                               width=15,
-                               values=["test", "test1", "test"],
-                               justify="right",
-                               state="readonly",
-                               )
-    choose_game.bind("<<ComboboxSelected>>", lambda e: main_w.focus())
-    choose_game.current(0)
-    choose_game.grid(row=0,
-                     column=2,
-                     sticky="n",
-                     )
-    back_scroll_button = Button(main_w,
-                             image=go_back_icon,
-                             highlightthickness=0,
-                             border=0,
-                             relief=tkinter.RIDGE,
-                             )
-    back_scroll_button.grid(row=0,
-                            column=0,
-                            sticky="w")
+
+def history_next_turn(back=False):
+    global HISTORY_TURN
+    if not back:
+        HISTORY_TURN += 1
+        try:
+            history_label.create_image(0, 0, image=history_list[HISTORY_TURN], anchor=NW)
+        except IndexError:
+            HISTORY_TURN -= 1
+    else:
+        if HISTORY_TURN > 0:
+            HISTORY_TURN -= 1
+            try:
+                history_label.create_image(0, 0, image=history_list[HISTORY_TURN], anchor=NW)
+            except IndexError:
+                HISTORY_TURN += 1
+
+
+def history_game_chosen():
+    global HISTORY_INDEX, HISTORY_TURN
+    HISTORY_INDEX = (history_choose_game.get()).strip("Game: ")
+    all_turns = os.listdir(f"history/Game{HISTORY_INDEX}")
+    HISTORY_TURN = 0
+    history_list.clear()
+    for index in all_turns:
+        path = f"history/Game{HISTORY_INDEX}/{index}"
+        history_list.append(tkinter.PhotoImage(file=path))
+    history_label.create_image(0, 0, image=history_list[HISTORY_TURN], anchor=NW)
 
 
 main_w = Tk()
@@ -296,6 +312,8 @@ history_icon = tkinter.PhotoImage(file="media/history_55x55.png")
 versus_icon = tkinter.PhotoImage(file="media/vs_ai_155x96.png")
 replay_icon = tkinter.PhotoImage(file="media/replay_130x130.png")
 go_back_icon = tkinter.PhotoImage(file="media/go_back_130x130.png")
+prev_turn_icon = tkinter.PhotoImage(file="media/prev_turn_100x100.png")
+next_turn_icon = tkinter.PhotoImage(file="media/next_turn_100x100.png")
 # main window - setup, size, centering experiments #
 # Was trying to do somewhat, flexible resolution for main_window, to adapt padding and window size for
 # different screen resolutions, but didn't work out as I wanted. Leaving fixed resolution and padding.
@@ -310,6 +328,7 @@ main_w.iconphoto(True, icon)
 main_w.title("TicTacToe")
 main_w.config(padx=145,
               pady=45, )
+main_w.protocol("WM_DELETE_WINDOW", lambda: (menu(), main_w.destroy()))
 # tiles setup #
 # I was thinking obviously making it by loop, but for this I would need coordinates and dictionary for them.
 # While I was thinking how to place and align them, already have made all of them.
@@ -465,4 +484,67 @@ versus_button.grid(row=1,
                    pady=(10, 0),
                    padx=(25, 0),
                    )
+# history image #
+history_label = Canvas(main_w,
+                       width=525,
+                       height=520)
+
+history_label.grid(row=0,
+                   column=1,
+                   sticky="nsew",
+                   )
+history_label.grid_remove()
+# history to menu #
+history_go_back_button = Button(main_w,
+                                image=go_back_icon,
+                                highlightthickness=0,
+                                border=0,
+                                relief=tkinter.RIDGE,
+                                command=menu,
+                                )
+history_go_back_button.grid(row=0,
+                            column=2,
+                            sticky="s")
+history_go_back_button.grid_remove()
+# History Combobox #
+style = ttk.Style()
+style.configure("TCombobox", fieldbackground="white", foreground="black")
+history_choose_game = ttk.Combobox(main_w,
+                                   width=12,
+                                   justify="center",
+                                   state="readonly",
+                                   font=("Helvetica", 10, "bold"),
+
+                                   )
+history_choose_game.option_add("*TCombobox*Listbox.Justify", "center")
+history_choose_game.bind("<<ComboboxSelected>>", lambda event: (main_w.focus(), history_game_chosen()))
+history_choose_game.grid(row=0,
+                         column=2,
+                         sticky="n",
+                         )
+history_choose_game.grid_remove()
+# History: previous turn button #
+history_prev_turn_button = Button(main_w,
+                                  image=prev_turn_icon,
+                                  highlightthickness=0,
+                                  border=0,
+                                  relief=tkinter.RIDGE,
+                                  command=lambda: history_next_turn(back=True),
+                                  )
+history_prev_turn_button.grid(row=0,
+                              column=0, )
+history_prev_turn_button.grid_remove()
+# History: next turn button #
+history_next_turn_button = Button(main_w,
+                                  image=next_turn_icon,
+                                  highlightthickness=0,
+                                  border=0,
+                                  relief=tkinter.RIDGE,
+                                  command=history_next_turn,
+                                  )
+history_next_turn_button.grid(row=0,
+                              column=2,
+                              sticky="w", )
+history_next_turn_button.grid_remove()
+
 main_w.mainloop()
